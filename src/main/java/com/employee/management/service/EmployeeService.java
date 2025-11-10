8090f1413aea789348ca3915d82f88921ede9dca
yuetl3mr <kimthach.work@gmail.com>
2025-11-10 08:55:53 +0700

    Module 7: Logging & Profiles - Add logging, configure profiles

--- src/main/java/com/employee/management/service/EmployeeService.java
+++ src/main/java/com/employee/management/service/EmployeeService.java
@@ -4,6 +4,8 @@ import com.employee.management.entity.Department;
 import com.employee.management.entity.Employee;
 import com.employee.management.exception.ResourceNotFoundException;
 import com.employee.management.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +16,8 @@ import java.util.Optional;
 @Service
 @Transactional
 public class EmployeeService {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeService.class);
 
     private final EmployeeRepository employeeRepository;
     private final DepartmentService departmentService;
@@ -29,7 +33,11 @@ public class EmployeeService {
     }
 
     public Employee saveEmployee(Employee employee) {
-        return employeeRepository.save(employee);
+        logger.info("Adding new employee: name={}, email={}, department={}", 
+                employee.getName(), employee.getEmail(), employee.getDepartment().getName());
+        Employee saved = employeeRepository.save(employee);
+        logger.info("Employee added successfully with ID: {}", saved.getId());
+        return saved;
     }
 
     public Optional<Employee> getEmployeeById(Long id) {
@@ -43,7 +51,11 @@ public class EmployeeService {
     }
 
     public Employee updateEmployee(Long id, Employee employeeDetails) {
+        logger.info("Updating employee with ID: {}", id);
         Employee employee = getEmployeeByIdOrThrow(id);
+        
+        String oldName = employee.getName();
+        String oldEmail = employee.getEmail();
         
         employee.setName(employeeDetails.getName());
         employee.setEmail(employeeDetails.getEmail());
@@ -51,14 +63,20 @@ public class EmployeeService {
             employee.setDepartment(employeeDetails.getDepartment());
         }
         
-        return employeeRepository.save(employee);
+        Employee updated = employeeRepository.save(employee);
+        logger.info("Employee updated: ID={}, oldName={}, newName={}, oldEmail={}, newEmail={}", 
+                id, oldName, updated.getName(), oldEmail, updated.getEmail());
+        return updated;
     }
 
     public void deleteEmployeeOrThrow(Long id) {
+        logger.info("Deleting employee with ID: {}", id);
         if (!employeeRepository.existsById(id)) {
+            logger.warn("Attempted to delete employee with ID {} but not found", id);
             throw new ResourceNotFoundException("Employee not found with id: " + id);
         }
         employeeRepository.deleteById(id);
+        logger.info("Employee deleted successfully with ID: {}", id);
     }
 
     public List<Employee> searchEmployeesByName(String name) {
